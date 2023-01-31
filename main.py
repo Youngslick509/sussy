3c00762f6c587500595ebcc810112c079f579442
Youngslick509 <brosorio@g.pateros.org>
2023-01-31 14:48:51 -0800

    yes

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 import random
 
+
+def evaluate(p1card, p2card, p1_deck, p2_deck, game_pile):
+   
+   
+   
+    import random
+
 deck = list(range(2, 15)) * 4
-random.shufflwe(deck)
+random.shuffle(deck)
 p1 = deck[:26]
 p2 = deck[26:]
 pile = []
 
 while len(p1) != 0 or len(p2) != 0:
 
-    p1card == p1_deck.pop()
-    p2card == p2_deck.pop()
+    p1 == deck.pop()
+    p2 == p2.pop()
 
-    if p1card == p1.pop[0]:
+    if p1 == deck.pop(0):
         p2card = p2.pop[0]
-        pile.apppend = (p1card)
+        pile.apppend = (p1)
         pile.apppend = (p2card)
 
-    print('player one has: ' + str(p1card) + '\nplayer two has: ' + str(p2card))
+    print('player one has: ' + str(p1) + '\nplayer two has: ' + str(p2))
 
-    if p1card > p2card:
+    if p1 > p2:
         print('player one wins!!!')
         p1.extend(pile)
         pile.clear()
 
-    elif p1card < p2card:
+    elif p1 < p2:
         print('player two wins!!!')
         p2.extend(pile)
         pile.clear()
     
-    elif p1card == p2card:
+    elif p1 == p2:
         print('war')
         pile.append(p1.pop(0))
         pile.append(p2.pop(0))
 
-if len(p2_deck) < 0:
+if len(p2) < 0:
     print('player two wins the game!!!!!!!!!!')
 else:
     print('player one wins tho')
